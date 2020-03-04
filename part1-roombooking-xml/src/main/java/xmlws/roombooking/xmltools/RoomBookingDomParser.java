9ca852ad64d91841fedfdd1098890b46bbb3edf8
LBCHTS <loic.beauchaints@iut-rodez.fr>
2020-03-04 18:25:57 +0100

    #1.2 Parser DOM a finir

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -1,10 +1,15 @@
 package xmlws.roombooking.xmltools;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 
 public class RoomBookingDomParser implements RoomBookingParser {
     @Override
     public RoomBooking parse(InputStream inputStream) {
-        return null;
+        RoomBooking roomBooking = new RoomBooking();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+        DocumentBuilderFactory dbf = 
     }
 }
