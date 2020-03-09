e1d81a975d9428da07591ead1b77ad31d79b409f
LBCHTS <loic.beauchaints@iut-rodez.fr>
2020-03-09 19:33:21 +0100

    fix #1.2 Parser DOM

--- part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
+++ part1-roombooking-xml/src/main/java/xmlws/roombooking/xmltools/RoomBookingDomParser.java
@@ -1,5 +1,8 @@
 package xmlws.roombooking.xmltools;
 
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
@@ -10,6 +13,28 @@ public class RoomBookingDomParser implements RoomBookingParser {
         RoomBooking roomBooking = new RoomBooking();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
 
-        DocumentBuilderFactory dbf = 
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(inputStream);
+            roomBooking.setRoomLabel(getValueForElementInDocument(Element.label,doc));
+            roomBooking.setUsername(getValueForElementInDocument(Element.username,doc));
+            roomBooking.setStartDate(sdf.parse(getValueForElementInDocument(Element.startDate,doc)));
+            roomBooking.setEndDate(sdf.parse(getValueForElementInDocument(Element.endDate,doc)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            roomBooking = null;
+        }
+        return roomBooking;
+    }
+
+    private String getValueForElementInDocument(Element element, Document doc) {
+        return doc.getElementsByTagName(element.name()).item(0).getTextContent();
+    }
+    private enum Element {
+        label,
+        username,
+        startDate,
+        endDate
     }
 }
